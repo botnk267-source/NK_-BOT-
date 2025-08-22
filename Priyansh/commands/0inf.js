e2774657430e2eaf133b9465cfc456eac848d8c4
botnk267-source <botnk267@gmail.com>
2025-08-22 08:42:32 +0530

    Update 0inf.js

--- Priyansh/commands/0inf.js
+++ Priyansh/commands/0inf.js
@@ -1,75 +1,67 @@
 module.exports.config = {
-  name: "info",
-  version: "1.0.1", 
-  hasPermssion: 0,
-  credits: "⑅⃝❥»̶̶͓͓͓̽̽̽»̶̶͓͓̽̽̽ℕ𝑲~ʌ̋̋̋̋̋̋̋̋̋̋̋̋̋̋̋̋̋̋𝑬𝑫𝑰𝑻𝑶𝑹~»̶̶͓͓͓̽̽̽⑅⃝✺❀✿＊",
-  description: "Admin and Bot info.",
-  commandCategory: "...",
-  cooldowns: 1,
-  dependencies: 
-  {
+	name: "info",
+	version: "1.0.1", 
+	hasPermssion: 0,
+	credits: "NK EDITOR BOT",
+	description: "Admin and Bot info.",
+	commandCategory: "...",
+	cooldowns: 1,
+	dependencies: 
+	{
     "request":"",
     "fs-extra":"",
     "axios":""
   }
 };
 module.exports.run = async function({ api,event,args,client,Users,Threads,__GLOBAL,Currencies }) {
-  const axios = global.nodemodule["axios"];
-  const request = global.nodemodule["request"];
-  const fs = global.nodemodule["fs-extra"];
-  const time = process.uptime(),
-      hours = Math.floor(time / (60 * 60)),
-      minutes = Math.floor((time % (60 * 60)) / 60),
-      seconds = Math.floor(time % 60);
-  const moment = require("moment-timezone");
-  var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【HH:mm:ss】");
-  var link =                                     
-  ["https://i.ibb.co/DDkk6qCv/IMG-20250804-WA0735.jpg"]; // DP लिंक यहाँ अपडेट किया गया है
-  var callback = () => api.sendMessage({
-    body:` 
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-【𝗔𝗗𝗠𝗜𝗡 𝗢𝗥 𝗕𝗢𝗧 𝗞𝗜 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗧𝗜𝗢𝗡 】
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-  ☄️ Bot Name☄️ $
-{global.config.BOTNAME}
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+const time = process.uptime(),
+		hours = Math.floor(time / (60 * 60)),
+		minutes = Math.floor((time % (60 * 60)) / 60),
+		seconds = Math.floor(time % 60);
+const moment = require("moment-timezone");
+var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【HH:mm:ss】");
+var link =                                     
+["https://i.ibb.co/DDkk6qCv/IMG-20250804-WA0735.jpg"];
+var callback = () => api.sendMessage({body:`
+╔════◇🌀◇════╗
+   💎 𝐁𝐎𝐓 ＩＮＦＯ 💎
+╚════◇🌀◇════╝
 
-╭──────╯🌙╰──────╮
-✨.     𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥.         ✨
-╰──────╮💫╭──────╯
-
-NAME: ☞ 💙⎯͢⎯⃝  𝑵𝑲 𝑬𝑫𝑰𝑻𝑶𝑹⎯͢⎯⃝💜🪽
-============================
-
-⋆✦✧✦⋆━━━ 👑 OWNER CONTACT LINK👑 ━━━⋆✦✧✦⋆
-
-     📘 Facebook   ➝   https://www.facebook.com/profile.php?id=61577417285926
-     📸 Instagram  ➝   https://www.instagram.com/nk_lovely_143_1?igsh=OXY4eDBsbzEzMnVr==
-     💬 Telegram   ➝   @NK2650
-
-⋆✦✧✦⋆━━━━━━━━━━━━━━━━━━━━━━━━━━⋆✦✧✦⋆
-🤖 𝗕𝗢𝗧 𝗣𝗥𝗘𝗙𝗜 🤖 ☞ . ☜ $
-{global.config.PREFIX}
-
-🤖𝗕𝗢𝗧🤖 𝗔𝗗𝗠𝗜𝗡👑
-
- 💙⎯͢⎯⃝  𝑵𝑲 𝑬𝑫𝑰𝑻𝑶𝑹⎯͢⎯⃝💜🪽
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-🍁UPTIME🍁
-🌪️Today is🌪️ ☞☜ღ${juswa}
-
-🌺Bot is running 🌺 ${hours}:${minutes}:${seconds}.
-
-💙Thanks for using $
-{global.config.BOTNAME}  
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
- 🍁👑𝗛𝗘𝗥𝗘 𝗜𝗦 𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥 𝗡𝗔𝗠𝗘 🍁👑
-            ╔═━━━━✦🖤✦━━━━═╗
-             🔥    𝑵𝑲 𝑬𝑫𝑰𝑻𝑶𝑹   🔥
-            ╚═━━━━✦🖤✦━━━━═╝
+🤖 𝘽𝙤𝙩 𝙉𝙖𝙢𝙚 ➤ ${global.config.BOTNAME} ⚡
+👑 𝙊𝙬𝙣𝙚𝙧 ➤ 𝑵𝑲 𝑬𝑫𝑰𝑻𝑶𝑹 👑🔥
+🎭 𝘼𝙠𝙖 ➤ 𝕋𝕙𝕖 𝕂𝕚𝕟𝕘 𝕆𝕗 𝔹𝕠𝕥𝕤 ✨
+╭───────────༺🌸༻───────────╮
+             💜    👇 𝑹𝒆𝒍𝒂𝒕𝒊𝒐𝒏𝒔𝒉𝒊𝒑. 👇💙
+╰───────────༺🌸༻───────────╯
 
 
-`,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")
-  }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
+❤️ 𝐍𝐊 𝐄𝐃𝐈𝐓𝐎𝐑 💞 + 💖 𝐍𝐢𝐡𝐚𝐫𝐢𝐤𝐚 𝐒𝐢𝐧𝐠𝐡 💖
+🌹 𝑭𝒐𝒓𝒆𝒗𝒆𝒓 𝑻𝒐𝒈𝒆𝒕𝒉𝒆𝒓 🌹
 
-  return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/juswa.jpg")).on("close",() => callback());
-};
+
+
+🚩 Jai Shree Ram 🚩
+╭━━━━━━━✦✗✦━━━━━━━╮
+🌐 𝐒𝐎𝐂𝐈𝐀𝐋 𝐌𝐄𝐃𝐈𝐀 🔗
+╰━━━━━━━✦✗✦━━━━━━━╯
+📘 Facebook ➤   https://www.facebook.com/share/1MhzLsBVRY/
+📸 Instagram ➤  https://www.instagram.com/nk_lovely_143_1?igsh=OXY4eDBsbzEzMnVr
+📡 Telegram ➤  t.me/NK2650
+
+╭━━━━━━━✦✗✦━━━━━━━╮
+📅 𝐓𝐨𝐝𝐚𝐲 ➤ ${juswa}  
+⏳ 𝐔𝐩𝐭𝐢𝐦𝐞 ➤ ${hours}:${minutes}:${seconds}  
+╰━━━━━━━✦✗✦━━━━━━━╯
+
+💠 𝐏𝐑𝐄𝐅𝐈𝐗 ➤ ${global.config.PREFIX}  
+⚡ 𝐒𝐓𝐀𝐓𝐔𝐒 ➤ ✅ RUNNING WITHOUT LIMITS 🚀🔥
+
+🎀 Thanks For Using ${global.config.BOTNAME} 🎀
+
+`,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/juswa.jpg")).on("close",() => callback());
+   };
+   
